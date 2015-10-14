0868bad3d61fe34ec908d4971a469d2f55e97977
Ptitmonstre <aubert.gwendal@gmail.com>
2015-10-14 15:33:25 +0200

    commit full folder

---
+++ src/FeedReader.java
@@ -0,0 +1,117 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import com.cybozu.labs.langdetect.Detector;
+import com.cybozu.labs.langdetect.DetectorFactory;
+import com.cybozu.labs.langdetect.LangDetectException;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+
+/**
+ * RSS Feed Reader
+ * 
+ * @author Aubert Gwendal & Scolan Alexis
+ *
+ */
+public class FeedReader {
+
+	/**
+	 * URL of the feed
+	 */
+	private URL url;
+	/**
+	 * Detector object
+	 */
+	private static Detector detector = null;
+
+	/**
+	 * Main fonction
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Proxy configuration
+		System.setProperty("http.proxySet", "true");
+		System.setProperty("http.proxyHost", "squidva.univ-ubs.fr");
+		System.setProperty("http.proxyPort", "3128");
+		System.setProperty("http.proxyType", "4");
+
+		if (args.length != 1)
+			System.exit(1);
+
+		// Setting the path for Langdetector
+		String dir = System.getProperty("user.dir");
+		System.out.println("current dir = " + dir);
+		try {
+			DetectorFactory.loadProfile(dir + "/profiles");
+		} catch (LangDetectException e1) {
+			e1.printStackTrace();
+		}
+
+		// URL du feed
+		try {
+			String line;
+			InputStream fis = new FileInputStream(dir+"/"+args[0]);
+			InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+			BufferedReader br = new BufferedReader(isr);
+			while ((line = br.readLine()) != null) {
+				URL feedUrl = new URL(line);
+				SyndFeedInput input = new SyndFeedInput();
+				SyndFeed feed = input.build(new XmlReader(feedUrl));
+				printFeed(feed);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Printer for the feed
+	 * 
+	 * @param feed
+	 */
+	private static void printFeed(SyndFeed feed) {
+		List<SyndEntry> entries = feed.getEntries();
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		// Loading profile for LangDetector
+		ObjectOutputStream o;
+		try {
+			o = new ObjectOutputStream(b);
+			for (SyndEntry e : entries) {
+				// Creating a hash of the message
+				o.writeObject(e);
+				System.out.print("Hash: " + b.toByteArray().toString());
+				System.out.print(", URL: " + e.getUri());
+				System.out.print(", Source: " + e.getSource());
+				System.out.print(", Date: " + e.getPublishedDate());
+				System.out.print(", Title: " + e.getTitle());
+				System.out.print(", Description: " + e.getDescription());
+
+				try {
+					// Detector creation and exploitation
+					detector = DetectorFactory.create();
+					detector.append(e.getUri() + " " + e.getTitle() + " " + e.getDescription());
+					System.out.print("Language : " + detector.detect() + "\n");
+				} catch (LangDetectException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+		} catch (IOException e2) {
+			System.err.println("Error creating the messages hashes");
+			e2.printStackTrace();
+		}
+	}
+
+}
---
+++ src/MRIEntry.java
@@ -0,0 +1,98 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MRIEntry{
+
+	private String title;
+	private String description;
+	private String author;
+	private String date;
+	private String url_src;
+	private String txt_src;
+	private String language;
+	private String copyright;
+	private String hash;
+	private MessageDigest md;
+	
+	public MRIEntry(String title, String description, String author, String date, String url_src, String txt_src,
+			String language, String copyright) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.author = author;
+		this.date = date;
+		this.url_src = url_src;
+		this.txt_src = txt_src;
+		this.language = language;
+		this.copyright = copyright;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			//cryptage en md5. Cle choisie titre + url + langue
+			byte[] toBytes = md.digest((title+url_src+language).getBytes());
+			hash = toBytes.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public MRIEntry(String hash){
+		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getUrl_src() {
+		return url_src;
+	}
+	public void setUrl_src(String url_src) {
+		this.url_src = url_src;
+	}
+	public String getTxt_src() {
+		return txt_src;
+	}
+	public void setTxt_src(String txt_src) {
+		this.txt_src = txt_src;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getCopyright() {
+		return copyright;
+	}
+	public void setCopyright(String copyright) {
+		this.copyright = copyright;
+	}
+	public String getHash() {
+		return hash;
+	}
+	public void setHash(String hash) {
+		this.hash = hash;
+	}	
+	
+}
